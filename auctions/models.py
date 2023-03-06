ed795a03d1e979b9943961e614cf441e1106a4b4
Kianggg <mkiang333@gmail.com>
2023-03-06 18:51:30 -0500

    Starting to work on the project

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,36 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True)
+    def __str__(self):
+        return f"{self.username}"
+
+class AuctionListing(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    startingBid = models.IntegerField(default=0)
+    image = models.URLField(max_length=200)
+    category = models.CharField(max_length=25)
+    description = models.CharField(max_length=280, default="Description of the listing")
+    active = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.title}"
+
+class Bid(models.Model):
+    id = models.AutoField(primary_key=True)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.bidder.username} bid ${self.amount}."
+    
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=280)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.bidder.username} bid ${self.amount}."
--- auctions/views.py
+++ auctions/views.py
@@ -3,13 +3,23 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django import forms
 
-from .models import User
+from .models import *
 
+# Form for creating a new listing
+class ListingForm(forms.Form):
+    title = forms.CharField(label='Listing Title', max_length=50)
+    startingBid = forms.IntegerField(label='Starting Bid')
+    image = forms.URLField(label='Image', max_length=200)
+    category = forms.CharField(label='Category', max_length=25)
+    active = forms.BooleanField(initial=True)
 
 def index(request):
-    return render(request, "auctions/index.html")
-
+    return render(request, "auctions/index.html", {
+        "listings": AuctionListing.objects.all()
+    })
 
 def login_view(request):
     if request.method == "POST":
@@ -61,3 +71,21 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+# Create a new listing
+@login_required  
+def create(request):
+    if request.method == "GET":
+        return render(request, "auctions/create.html")
+    elif request.method == "POST":
+        return render(request, "auctions/create.html")
+    else:
+        return HttpResponseRedirect(reverse("index"))
+    
+# View an existing listing
+@login_required  
+def listing(request, listing_id):
+    listing = AuctionListing.objects.get(id=listing_id)
+    return render(request, "auctions/listing.html", {
+        "listing": listing
+    })
